b16090907d04e978bd47f7599157b38e08501815
erno <erno.johansson@gmail.com>
2014-01-22 15:47:48 +0200

    turhia tiedostoja poistettu gitista

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Boksi/auxilo-box
-# Build directory: /home/pi/Boksi/auxilo-box
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
